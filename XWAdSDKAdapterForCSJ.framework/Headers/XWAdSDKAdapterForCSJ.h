39e6f0bbf5dcf78b86c2b1d4925e1c45b9aca235
laole918 <laole918@qq.com>
2021-10-14 18:26:53 +0800

    first commit

---
+++ XWAdSDKAdapterForCSJ.framework/Headers/XWAdSDKAdapterForCSJ.h
@@ -0,0 +1,18 @@
+//
+//  XWAdSDKAdapterForCSJ.h
+//  XWAdSDKAdapterForCSJ
+//
+//  Created by laole918 on 2021/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAdSDKAdapterForCSJ.
+FOUNDATION_EXPORT double XWAdSDKAdapterForCSJVersionNumber;
+
+//! Project version string for XWAdSDKAdapterForCSJ.
+FOUNDATION_EXPORT const unsigned char XWAdSDKAdapterForCSJVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAdSDKAdapterForCSJ/PublicHeader.h>
+
+
